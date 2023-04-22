749b7582667ca2d75fd2c44be840fdd5b18a1d3f
hectorbenitez19 <beniez.h@husky.neu.edu>
2023-04-22 00:20:37 -0400

    added initial signup code
    added the framework for signup but is not fully implemented

--- src/signup/index.js
+++ src/signup/index.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import * as userService from "../services/users-service";
+import { useNavigate } from "react-router-dom";
 import "./signup.css";
 
-const SignUpComponent = () => {
+function SignUpComponent() {
+  const [user, setUser] = useState({
+    username: "alice",
+    password: "alice",
+    firstName: "Alice",
+    lastName: "Wonderland",
+    email: "alice123@gmail.com",
+    profilePic: "",
+    createdOn: Date.now,
+  });
+  const navigate = useNavigate();
+  const register = async () => {
+    await userService.register(user);
+    navigate("/profile");
+  };
 return(
-
 <>
-<form >
   <div className="container">
     <h1>Sign Up</h1>
     <p>Please fill in this form to create an account.</p>
-
+   <label for="fname"><b>First Name</b></label>
+        <input type="text" className="form-control" placeholder="Enter First Name" name="fname" value={user.firstName}
+                                                                                                        onChange={(e) => setUser({ ...user, firstName: e.target.value })} required/>
+   <label for="lname"><b>First Name</b></label>
+        <input type="text" className="form-control" placeholder="Enter Last Name" name="lname" value={user.lastName}
+                                                                                                        onChange={(e) => setUser({ ...user, lastName: e.target.value })} required/>
 
     <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required/>
+    <input type="text" className="form-control" placeholder="Enter Email" name="email" value={user.email}
+                                                                                         onChange={(e) => setUser({ ...user, email: e.target.value })} required/>
+
+       <label for="uname"><b>Username</b></label>
+        <input type="text" className="form-control" placeholder="Enter Username" name="uname" value={user.username}
+                                                                                                        onChange={(e) => setUser({ ...user, username: e.target.value })} required/>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required/>
-
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required/>
+    <input type="password" className="form-control" placeholder="Enter Password" name="psw"   value={user.password}
+                                                                                                     onChange={(e) => setUser({ ...user, password: e.target.value })} required/>
 
     <label>
       <input type="checkbox" checked="checked" name="remember" style={{marginBottom: "15px"}}/> Remember me
@@ -28,11 +50,10 @@ return(
 
     <div className="clearfix">
       <button type="button" className="cancelbtn">Cancel</button>
-      <button type="submit" className="signupbtn">Sign Up</button>
+      <button type="submit" onClick={register}  className="signupbtn">Sign Up</button>
     </div>
 
   </div>
-</form>
 </>
 );
 
