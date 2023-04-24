9a0a51fe300b5f9d1d311799bb5860a95418d99a
emilyhendrick <emhendrick31@gmail.com>
2023-04-23 22:10:56 -0400

    update yelp services

--- src/yelp/yelp-service.js
+++ src/yelp/yelp-service.js
@@ -1,27 +1,37 @@
 import axios from "axios";
-export const YELP_API = "https://api.yelp.com/v3";
+export const YELP_API = "https://api.yelp.com/v3/businesses";
 export const YELP_KEY = process.env.REACT_APP_YELP_API_KEY;
+export const LOCAL_RESTAURANT_API = "http://localhost:4000/api/restaurant";
+export const CORS_LINK = 'https://cors-anywhere.herokuapp.com/';
+
+// **** IMPORTANT ****
+// For access to the Yelp API via axios, visit https://cors-anywhere.herokuapp.com/corsdemo
+// Click the button to request temporary access and then it should work
 
 const api = axios.create({
-    baseURL: YELP_API,
-    headers: {
-        Authorization: `Bearer ${YELP_KEY}`,
-    },
+    withCredentials: true,
 });
 
 export const searchRestaurants = async (location, term) => {
-    const response = await api.get("/businesses/search", {
-        params: {
-            location: location,
-            term: term,
-        },
-    });
+    // change the limit to however many we want to display, rn it's 20
+    const response = await axios.get(
+        `${CORS_LINK}${YELP_API}/search?term=${query}&sort_by=best_match&limit=20&location=${location}`,
+        { headers: { Authorization: `Bearer ${YELP_KEY}` } }
+    );
     const json = await response.data;
     return json.businesses;
 };
 
 export const getRestaurantDetails = async (restaurantId) => {
-    const response = await api.get(`/businesses/${restaurantId}`);
+    const response = await axios.get(
+        `${CORS_LINK}${YELP_API}/${restaurantId}`
+    );
     const json = await response.data;
     return json;
+};
+
+// pull search results from business in our local db
+export const getLocalRestaurants = async (query) => {
+    const response = await api.get(`${LOCAL_RESTAURANT_API}/${query}`);
+    return response.data;
 };
