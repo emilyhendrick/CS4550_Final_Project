1077a6b58ef57192d2580b3d2954ab5a20e9b7da
hectorbenitez19 <beniez.h@husky.neu.edu>
2023-04-26 19:34:25 -0400

    Update whats-happening.js
    fixed rating should now work

--- src/restaurant/whats-happening.js
+++ src/restaurant/whats-happening.js
@@ -1,31 +1,34 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {createReview} from "./review-reducer";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Link, useNavigate, useParams}
   from "react-router-dom";
 import * as reviewService from "../services/review-service";
 import {createReviewThunk} from "../services/review-thunk";
+import {logoutThunk, profileThunk} from "../services/user-thunks";
+import * as userService from "../services/users-service";
 
 const WhatsHappening = () => {
  const {username} = useParams();
- //const {currentUser} = useSelector((state) => state.users);
+ const [profile, setProfile] = useState({});
  let [whatsHappening, setWhatsHappening] = useState('');
  let [userRating, setUserRating] = useState(1);
   const dispatch = useDispatch();
   let [review, setReview] = useState({
           review: "",
-          rating: 0,
+          rating: 1,
           reviewer: username,
           profilePic: "https://wallpapers.com/images/hd/aesthetic-profile-picture-pjnvodm0tj798j1q.jpg",
           createdOn: Date.now(),
-          restaurantName: "Thorntons",
-  })
+          restaurantName: username,
+  });
+
 
  const reviewClickHandler = async () => {
     const newReview = {
       review: whatsHappening,
       rating: userRating,
-      reviewer: username,
+      reviewer: profile.username,
       profilePic: "https://wallpapers.com/images/hd/aesthetic-profile-picture-pjnvodm0tj798j1q.jpg",
       createdOn: Date.now(),
       restaurantName: "Thorntons",
@@ -35,6 +38,7 @@ const WhatsHappening = () => {
    console.log(whatsHappening);
  }
 
+
  return (
  <>
  <form>
@@ -43,18 +47,30 @@ const WhatsHappening = () => {
        <img src="/images/user.jpg" width={60}/>
      </div>
      <div className="col-11">
-       <textarea value={review.whatsHappening} placeholder="Leave A Review?"
+       <textarea value={review.review} placeholder="Leave A Review?"
                className="form-control border-0"
                onChange={(event) => setReview({...review, review: event.target.value})}>
        </textarea>
+       <h4>Rating:</h4>
+        <div>
+          <input type="radio" value="Personal"
+                name="radio-rating" id="radio-one" onChange={(e) => setReview({...review, rating: 1})} required/>
+          <label for="radio-one">One</label><br/>
+          <input type="radio" value="Business"
+                name="radio-rating" id="radio-two" onChange={(e) => setReview({...review, rating: 2})} required/>
+          <label for="radio-two">Two</label><br/>
 
-       <select>
-          <option value={review.userRating} onChange={(event) => setReview({...review, rating: 1})}>1</option>
-          <option value={review.userRating} onChange={(event) => setReview({...review, rating: 2})}>2</option>
-          <option value={review.userRating} onChange={(event) => setReview({...review, rating: 3})}>3</option>
-          <option value={review.userRating} onChange={(event) => setReview({...review, rating: 4})}>4</option>
-          <option value={review.userRating} onChange={(event) => setReview({...review, rating: 5})}>5</option>
-       </select>
+          <input type="radio" value="Personal"
+                name="radio-rating" id="radio-three" onChange={(e) => setReview({...review, rating: 3})} required/>
+          <label for="radio-three">Three</label><br/>
+          <input type="radio" value="Business"
+                name="radio-rating" id="radio-four" onChange={(e) => setReview({...review, rating: 4})} required/>
+          <label for="radio-four">Four</label><br/>
+          <input type="radio" value="Personal"
+                name="radio-rating" id="radio-five" onChange={(e) => setReview({...review, rating: 5})} required/>
+          <label for="radio-five">Five</label><br/>
+       </div>
+
        <div>
          <button type="submit" className="rounded-pill btn btn-primary float-end mt-2 ps-3 pe-3 fw-bold"
                  onClick={reviewClickHandler}>
