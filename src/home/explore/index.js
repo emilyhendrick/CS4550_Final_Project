9bb6c213ec675784396d50c53eb186fdfc804fb4
Malick Tobe <tobe.m@northeastern.edu>
2023-04-26 00:21:03 -0400

    added login/logout to home page

--- src/home/explore/index.js
+++ src/home/explore/index.js
@@ -7,9 +7,11 @@ import YelpSearchScreen from "../../yelp/yelp-search";
 import {loginThunk, logoutThunk} from "../../services/user-thunks";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
+import {useSelector} from "react-redux";
 
 import "./index.css";
 import {useState} from "react";
+
 
 const ExploreComponent = () => {
     const user = useState({
@@ -18,49 +20,51 @@ const ExploreComponent = () => {
         firstName: "Test",
         lastName: "User",
     });
+    const { currentUser } = useSelector((state) => state.users);
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleLogin = async () => {
         try {
             await dispatch(loginThunk(user));
-            navigate("/profile");
+            navigate("/login");
         } catch (e) {
             alert(e);
         }
     };
 
-    return(
+    return (
         <>
             <div className="row">
-                {/*<div className="col-9 position-relative">*/}
-                {/*    <input placeholder="Search Restaurant"*/}
-                {/*           className="form-control rounded-pill ps-5"/>*/}
-                {/*    <i className="bi bi-search position-absolute*/}
-                {/*       wd-nudge-up"></i>*/}
-                {/*</div>*/}
-                <div className="col-2" align="right">
-                {/*    <button type="submit" className="btn btn-primary">Log in</button> &thinsp;*/}
-                {/*    <button type="submit" className="btn btn-danger">Log Out</button>*/}
-                    <button type="submit" onClick={handleLogin}>Login</button>
-                </div>
+                {currentUser ? (
+                    <div className="col-2" align="right">
+                        <button onClick={() => dispatch(logoutThunk())}>Logout</button>
+                    </div>
+                ) : (
+                    <div className="col-2" align="right">
+                        <button onClick={handleLogin}>Login</button>
+                    </div>
+                )}
             </div>
             <YelpSearchScreen/>
             <YelpRestaurantDetails/>
-            {/*<h3 align="center">Recent review about your business</h3>*/}
-            {/*<RecentReview/>*/}
+            {currentUser ? (
+                <>
+                    <h3 align="center">Your most recent review</h3>
+                    <RecentReview />
+                </>
+            ) : null}
             <h3 align="center">Categories</h3>
-
-            <WhoToFollowList/>
-            <br/>
-
+            <WhoToFollowList />
+            <br />
             <div className="position-relative mb-2">
-                <img src="/images/bosrestaurant.jpg" className="w-100" alt='Starship'/>
+                <img src="/images/bosrestaurant.jpg" className="w-100" alt="Starship" />
                 <h1 className="position-absolute wd-nudge-up text-white">
-                    Explore restaurants in your area</h1>
+                    Explore restaurants in your area
+                </h1>
             </div>
-            <br/>
+            <br />
             <h3 align="center">Recent Reviews</h3>
-            <PostSummaryList/>
+            <PostSummaryList />
         </>
     );
 };
--- src/home/index.js
+++ src/home/index.js
@@ -9,26 +9,19 @@ import { configureStore } from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
 import HomeComponent from "./home";
 import ExploreComponent from "./explore";
+import store from "../reducers/store";
 
-const store = configureStore({reducer: {who: whoReducer, tuitsData: tuitsReducer}});
+// const store = configureStore({reducer: {who: whoReducer, tuitsData: tuitsReducer}});
 
 
 function Home() {
     return (
         <Provider store={store}>
         <div className="row mt-2">
-            {/*<div className="col-2 col-md-2 col-lg-1 col-xl-2">*/}
-            {/*    <NavigationSidebar active="home"/>*/}
-            {/*</div>*/}
             <div className="col-12 col-md-12 col-lg-12 col-xl-12"
                  style={{"position": "relative"}}>
                 <ExploreComponent/>
-             {/*<HomeComponent/>*/}
             </div>
-
-            {/*<div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">*/}
-            {/*    <WhoToFollowList/>*/}
-            {/*</div>*/}
         </div>
         </Provider>
     );
